564fe63b6564e81d848d9f2f5ecb862f637e59b8
DarkSky3309 <lust3309@gmail.com>
2023-02-08 15:37:27 +0300

    add figure to board

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,11 @@
 import {Cell} from "./Cell";
 import {Colors} from "./Colors";
+import {Pawn} from "./figures/Pawn";
 import {Queen} from "./figures/Queen";
+import {King} from "./figures/King";
+import {Knight} from "./figures/Knight";
+import {Bishop} from "./figures/Bishop";
+import {Rook} from "./figures/Rook";
 
 export class Board {
     cells: Cell[][] = []
@@ -20,10 +25,55 @@ export class Board {
         }
     }
 
-    public getCell(x:number, y:number){
+    private addPawns() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1))
+            new Pawn(Colors.WHITE, this.getCell(i, 6))
+        }
+    }
+
+    private addQueen() {
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+        new Queen(Colors.WHITE, this.getCell(3, 7))
+    }
+
+    private addKing() {
+        new King(Colors.BLACK, this.getCell(4, 0))
+        new King(Colors.WHITE, this.getCell(4, 7))
+    }
+
+    private addKnights() {
+        new Knight(Colors.BLACK, this.getCell(1, 0))
+        new Knight(Colors.WHITE, this.getCell(6, 7))
+        new Knight(Colors.BLACK, this.getCell(6, 0))
+        new Knight(Colors.WHITE, this.getCell(1, 7))
+    }
+
+    private addRooks() {
+        new Rook(Colors.BLACK, this.getCell(0, 0))
+        new Rook(Colors.WHITE, this.getCell(7, 7))
+        new Rook(Colors.BLACK, this.getCell(7, 0))
+        new Rook(Colors.WHITE, this.getCell(0, 7))
+    }
+
+    private addBishops() {
+        new Bishop(Colors.BLACK, this.getCell(2, 0))
+        new Bishop(Colors.WHITE, this.getCell(5, 7))
+        new Bishop(Colors.BLACK, this.getCell(5, 0))
+        new Bishop(Colors.WHITE, this.getCell(2, 7))
+    }
+
+
+    public getCell(x: number, y: number) {
         return this.cells[y][x]
     }
+
     public addFigures() {
-        new Queen(Colors.WHITE, this.getCell(3,3))
+        this.addBishops();
+        this.addKing();
+        this.addQueen();
+        this.addPawns();
+        this.addRooks();
+        this.addKnights()
     }
 }
